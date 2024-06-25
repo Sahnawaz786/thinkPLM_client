4716d4b66f5b6e50d2c6bdf38b4c7ede99cf2ced
Sahnawaz786 <shanawajalam20@gmail.com>
2024-06-25 17:00:52 +0530

    Fixed: popup UI

--- src/Components/LeftBar.jsx
+++ src/Components/LeftBar.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { FaArrowRight } from 'react-icons/fa';
-import { ImCross } from 'react-icons/im';
-import { Link, useLocation } from 'react-router-dom';
-import SupplierServices from '../services/supplier.services';
-import styles from '../style.module.css';
+import React, { useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { Link, useLocation } from "react-router-dom";
+import SupplierServices from "../services/supplier.services";
+import styles from "../style.module.css";
+import { openNewWindow, URL } from "../utils/helper";
 
 const LeftBar = () => {
-  const {getSupplier} = new SupplierServices();
-  const {pathname} = useLocation();
+  const { getSupplier } = new SupplierServices();
+  const { pathname } = useLocation();
 
   const [data, setData] = useState([]);
 
-  const [isDataUpdated,setIsDataUpdated] = useState(false);
+  const [isDataUpdated, setIsDataUpdated] = useState(false);
 
   const [display, setDisplay] = useState(true);
 
@@ -22,7 +23,9 @@ const LeftBar = () => {
   const handleNavToggle = (elem) => {
     setData((data) =>
       data?.map((item) =>
-        item === elem ? { ...item, isVisible: !item.isVisible } : {...item,isVisible:''}
+        item === elem
+          ? { ...item, isVisible: !item.isVisible }
+          : { ...item, isVisible: "" }
       )
     );
   };
@@ -31,12 +34,12 @@ const LeftBar = () => {
     const data = await getSupplier();
     const newData = data?.data?.map((elem) => ({ ...elem, isVisible: false }));
     setData(newData.reverse());
-    console.log('suppliers', data);
-};
+    console.log("suppliers", data);
+  };
 
   useEffect(() => {
     handleAPI();
-  },[]);
+  }, []);
 
   return (
     <div className={styles.leftbar}>
@@ -48,8 +51,8 @@ const LeftBar = () => {
           >
             <div className={styles.logoText}>
               <img
-                src='/images/supplier.png'
-                alt='logo'
+                src="/images/supplier.png"
+                alt="logo"
                 className={styles.imageIcon}
               />
               <span>SUPPLIER</span>
@@ -61,41 +64,57 @@ const LeftBar = () => {
           </div>
 
           <div className={styles.leftbarParent}>
-            <div className={styles.logoText} style={{ marginBottom: '10px' }}>
+            <div className={styles.logoText} style={{ marginBottom: "10px" }}>
               <img
-                src='/images/supplier.png'
-                alt='logo'
+                src="/images/supplier.png"
+                alt="logo"
                 className={styles.imageIcon}
               />
-              <Link
+              {/* <Link
                 style={{ textDecoration: 'none', color: 'black' }}
                 to={'/create-supplier'}
               >
                 Create Supplier ➕
-              </Link>
+              </Link> */}
+              <span
+                style={{ cursor: "pointer" }}
+                onClick={(e) => openNewWindow(e, `${URL}/create-supplier`)}
+              >
+                Create Supplier ➕
+              </span>
             </div>
 
             {data?.map((elem, index) => {
               return (
-                <div  key={index}>
+                <div key={index}>
                   <div className={styles.leftbarChild}>
                     <div className={styles.logoText}>
                       <img
-                        src='/images/supplier.png'
-                        alt='logo'
+                        src="/images/supplier.png"
+                        alt="logo"
                         className={styles.imageIcon}
                       />
 
-                      <span title={elem.category} onClick={() => handleNavToggle(elem)}>
+                      <span
+                        title={elem.category}
+                        onClick={() => handleNavToggle(elem)}
+                      >
                         {elem?.name}
                       </span>
                     </div>
                     {elem?.isVisible ? (
                       <div className={styles.leftbarOptions}>
-                        <Link to={`/supplier-details/${elem?.id}`}>Details</Link>
+                        <Link to={`/supplier-details/${elem?.id}`}>
+                          Details
+                        </Link>
+                        <Link to="/contract-details">Part Management</Link>
+                        <Link to="/contract-details">Document Management</Link>
+
                         <Link to={`/user-management`}>User Management</Link>
-                        <Link to={'/onboard-status'}>Onboarding status</Link>
-                        <Link to='/contract-details'>View Contract details</Link>
+                        <Link to={"/onboard-status"}>Onboarding status</Link>
+                        <Link to="/contract-details">
+                          View Contract details
+                        </Link>
                       </div>
                     ) : (
                       <></>
